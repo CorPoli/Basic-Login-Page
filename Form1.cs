b6e5a0de5c0299684448b2be226350f0434bccf1
CorPoli <corpolistore@gmail.com>
2025-11-30 15:52:55 +0000

    Form1.cs
    Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+namespace YOUR_PRODJECT_NAME_HERE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Key_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ENTER_Click(object sender, EventArgs e)
+        {
+            if (Key.Text == "1234")
+            {
+                MessageBox.Show("Active Key", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                DashBoard dash = new DashBoard();
+                dash.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Key!", "Access Denied",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
